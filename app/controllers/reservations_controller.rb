b3559a965fa3b1af30bfe326eaf784cf4ca933ec
ITO-T <ITO-T@helo.com>
2018-11-21 16:54:06 +0900

    rubocop

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,41 +8,41 @@ class ReservationsController < ApplicationController
     @color = MenuContent.where(menu_id: 3)
     @mon = params[:month]
     @day = params[:day]
-    #予約済み時間
+    # 予約済み時間
     reserved_time = []
-    #予約時間
+    # 予約時間
     reservation_time_default = [
-      "10:00",
-      "10:30",
-      "11:00",
-      "11:30",
-      "12:00",
-      "12:30",
-      "13:00",
-      "13:30",
-      "14:00",
-      "14:30",
-      "15:00",
-      "15:30",
-      "16:00",
-      "16:30",
-      "17:00",
-      "17:30",
-      "18:00",
-      "18:30"
+      '10:00',
+      '10:30',
+      '11:00',
+      '11:30',
+      '12:00',
+      '12:30',
+      '13:00',
+      '13:30',
+      '14:00',
+      '14:30',
+      '15:00',
+      '15:30',
+      '16:00',
+      '16:30',
+      '17:00',
+      '17:30',
+      '18:00',
+      '18:30'
     ]
     if @mon && @day
-      #検索する年月日
-      search_date = Time.new.year.to_s + "-" + @mon.to_s + "-" + @day.to_s
-      #search_dateを元にその年月日の予約を取り出す
+      # 検索する年月日
+      search_date = Time.new.year.to_s + '-' + @mon.to_s + '-' + @day.to_s
+      # search_dateを元にその年月日の予約を取り出す
       @reservation = Reservation.select('start_time').where(start_time: search_date.in_time_zone.all_day)
-      #予約済みの時間を取り出す
+      # 予約済みの時間を取り出す
       @reservation.each do |r|
-      reserved_time << r.start_time.hour.to_s + ":" + r.start_time.min.to_s
+        reserved_time << r.start_time.hour.to_s + ':' + r.start_time.min.to_s
       end
-      #予約済み時間を引く
+      # 予約済み時間を引く
       reservation_time = reservation_time_default - reserved_time
-      render :json => {time: reservation_time}
+      render json: { time: reservation_time }
     end
   end
 
