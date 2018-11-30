37557730b4641bbd387920b7954326922bb628d4
ITO-T <ITO-T@helo.com>
2018-11-30 17:05:27 +0900

    days error

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -8,45 +8,57 @@ $(function(){
   // 月が変更されたらvalue属性の値を取り出す
   $("#month").change(function(){
     var month = $("#month").val();
+    var day = $("#day").val();
     console.log(month);
-    getTimes();
+    getTimes(month, day);
   });
 
   // 日が変更されたらvalue属性の値を取り出す
   $("#day").change(function(){
+    var month = $("#month").val();
     var day = $("#day").val();
     console.log(day);
-    getTimes();
+    getTimes(month, day);
   });
 
-  function getTimes(){
+  function getTimes(month, day){
     $.ajax({
       url: "/reservations",
       type: "GET",
       data: { 
-        month: $("#month").val(),
-        day: $("#day").val()
+        month: month,
+        day: day
       },
       success: function(data){
         console.log(data.time);
-        var day_option = "";
-        for (var i = 0; i < data.day.length; i++){
-          if data.days[i] == data.day{
-            day_option += '<option value="' + data.days[i] + ' selected">' + data.days[i] + '</option>';
-          }else{
-            day_option += '<option value="' + data.days[i] + '">' + data.days[i] + '</option>';
-          }
-        };
-        $(".select_day").html(day_option);
+        //var day_option = "";
+        //for (var i = 0; i < data.day.length; i++){
+        ////  if data.days[i] == data.day{
+        //  //  day_option += '<option value="' + data.days[i] + ' selected">' + data.days[i] + '</option>';
+        // // }else{
+        //    day_option += '<option value="' + data.days[i] + '">' + data.days[i] + '</option>'
+        //  }
+        //}
+        //$(".select_day").html(day_option);
         var time_option = "";
         for (var i = 0; i < data.time.length; i++){
-          time_option += '<option value="' + data.time[i] + '">' + data.time[i] + '</option>';
-        };
+          time_option += '<option value="' + data.time[i] + '">' + data.time[i] + '</option>'
+        }
         $(".select_time").html(time_option);
+        var day_option = "";
+        for (var i = 0; i < data.day.length; i++){
+          if (data.days[i] == data.day){
+            day_option += '<option value="' + data.days[i] + ' selected">' + data.days[i] + '</option>'
+          }else{
+            day_option += '<option value="' + data.days[i] + '">' + data.days[i] + '</option>'
+          }
+        }
+        $(".select_day").html(day_option);
+        console.log(day_option);
       },
       error: function(data){
         console.log("error");
-      };
+      }
     });
   };
 
@@ -95,37 +107,6 @@ $(function(){
 //    marker.addListener('click', function() {
 //      infowindow.open(map, marker);
 //    });
-//  }
-
-
-
-// -------- ページ内リンク -------
-  // URLハッシュ#取得
-//  var urlHash = location.hash;
-//  // ハッシュ#あればページ内スクロール
-//  if(urlHash){
-//    // スクロールを０に戻す stop()->動きを止める
-//    $("body,html").stop().scrollTop(0);
-//    setTimeout(function(){
-//      // ロード時の処理待ち、時間差でスクロール
-//      scrollToAnker(urlHash);
-//    }, 100);
-//  }
-//  $("a[href^='#']").click(function(){
-//    // ぺージ内リンク先取得
-//    var href = $(this).attr("href");
-//    // リンク先が#かからだったらhtmlに
-//    var hash = href == "#" || href == "" ? "html" : href;
-//    // スクロール実行
-//    scrollToAnker(hash);
-//    // リンク無効化
-//    return false;
-//  })
-//
-//  function scrollToAnker(hash){
-//    var target = $(hash);
-//    var position = target.offset().top;
-//    $("body,html").stop().animate({scrollTop:position}, 500);
 //  }
 });
 
